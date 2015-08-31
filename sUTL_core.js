21ffc539d697443d78c147412bd3572dee9785e1
Emlyn O'Regan <emlynoregan@gmail.com>
2015-08-31 19:56:23 +0930

    Update sUTL_core.js

--- sUTL_core.js
+++ sUTL_core.js
@@ -1,68 +1,82 @@
 distributions.push(
-[
-  {
-    "name": "example_core_emlynoregan_com",
-    "language": "sUTL0",
-    "transform-t": "Core: Example"
-  },
-  {
-    "name": "example2_core_emlynoregan_com",
-    "language": "sUTL0",
-    "transform-t": [ "#*.example_core", "#*.example_core" ],
-    "test-t": {
-      "&": "if",
-      "cond": {"'": 
-        {
-          "&": "!=",
-          "a": "#*.example_core",
-          "b": "Core: Example"
+  [
+    {
+      "name": "example_core_emlynoregan_com",
+      "language": "sUTL0",
+      "transform-t": "Core: Example"
+    },
+    {
+      "name": "example2_core_emlynoregan_com",
+      "language": "sUTL0",
+      "transform-t": [ "#*.example_core", "#*.example_core" ],
+      "test-t": {
+        "&": "if",
+        "cond": {"'": 
+          {
+            "&": "!=",
+            "a": "#*.example_core",
+            "b": "Core: Example"
+          }
+        },
+        "true": "example_core behaves unexpectedly",
+        "false": null
+      },
+      "requires": ["example_core"]
+    },
+    {
+      "name": "example3fails_core_emlynoregan_com",
+      "language": "sUTL0",
+      "transform-t": "Core: Example 3",
+      "test-t": [
+        "Example 3 Fails"
+      ]
+    },
+    {
+      "name": "map_core_emlynoregan_com",
+      "language": "sUTL0",
+      "transform-t": 
+      {
+        "&": "if",
+        "cond": "#@.list",
+        "true": {
+          "'": [
+            "&&",
+            {
+              "!": "#@.t",
+              "item": {
+                "!": "#*.head_core_emlynoregan_com",
+                "in": "#@.list"
+              }
+            },
+            {
+              "!": "#*.map_l_emlynoregan_com",
+              "list": {
+                "!": "#*.tail_core_emlynoregan_com",
+                "in": "#@.list"
+              },
+              "t": "#@.t"
+            }
+          ]
+        },
+        "false": {
+          "'": []
         }
       },
-      "true": "example_core behaves unexpectedly",
-      "false": null
+      "requires": [
+        "map_core_emlynoregan_com", 
+        "head_core_emlynoregan_com", 
+        "tail_core_emlynoregan_com"
+      ]
     },
-    "requires": ["example_core"]
-  },
-  {
-    "name": "example3fails_core_emlynoregan_com",
-    "language": "sUTL0",
-    "transform-t": "Core: Example 3",
-    "test-t": [
-      "Example 3 Fails"
-    ]
-  },
-  {
-    "name": "map_l_emlynoregan_com",
-    "language": "sUTL0",
-    "transform-t": 
     {
-      "&": "if",
-      "cond": "#@.list",
-      "true": {
-        "'": [
-          "&&",
-          {
-            "!": "#@.t",
-            "item": {
-              "!": "#$.head",
-              "in": "#@.list"
-            }
-          },
-          {
-            "!": "#*.map_l_emlynoregan_com",
-            "list": {
-              "!": "#$.tail",
-              "in": "#@.list"
-            },
-            "t": "#@.t"
-          }
-        ]
-      },
-      "false": {
-        "'": []
-      }
+      "name": "head_core_emlynoregan_com",
+      "language": "sUTL0",
+      "transform-t": "#@.in[0]"
     },
-    "requires": ["map_l_emlynoregan_com"]
-  }
-]
+    {
+      "name": "tail_core_emlynoregan_com",
+      "language": "sUTL0",
+      "transform-t": "##@.in[1:]"
+    }
+  ]
 )
