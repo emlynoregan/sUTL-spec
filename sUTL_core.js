bd8827552c674b208b2646d839c77cf9e69230d7
Emlyn O'Regan <emlynoregan@gmail.com>
2015-09-03 21:57:51 +0930

    fix

--- sUTL_core.js
+++ sUTL_core.js
@@ -315,11 +315,11 @@ distributions.push(
         "false": []
       },
       "requires": [
-        "zip_core_emlynoregan.com", 
         "count_core_emlynoregan.com", 
         "require_core_emlynoregan.com",
         "head_core_emlynoregan.com", 
-        "tail_core_emlynoregan.com"
+        "tail_core_emlynoregan.com",
+        "zip_core_emlynoregan.com"
       ]
     }
   ]
